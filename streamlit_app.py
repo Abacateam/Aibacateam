f3ef8b9f36ad7735e4677fa6fcc9586a3b60dcdb
Abacateam <abacateam2024@gmail.com>
2024-09-14 20:42:09 -0300

    Teste Api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
+import requests
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# URL da API
+api_url = "https://intranet.portodesantos.com.br/_json/porto_hoje.asp?tipo=programados2"
+
+# Função para consumir a API
+def get_data_from_api(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error("Falha ao obter os dados da API.")
+        return None
+
+# Função principal do Streamlit
+def main():
+    st.title("Dados do Porto de Santos - Programação de Navios")
+    
+    # Botão para carregar os dados
+    if st.button("Carregar Dados"):
+        data = get_data_from_api(api_url)
+        
+        if data:
+            # Transformando os dados em um DataFrame para visualização
+            df = pd.DataFrame(data)
+            st.dataframe(df)
+        else:
+            st.error("Nenhum dado encontrado.")
+
+if __name__ == "__main__":
+    main()
